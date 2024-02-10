d8d50f64463d13c254932e7f659ac7250e53487c
MohammedToshif <toshif92612@gmail.com>
2024-02-10 13:02:38 +0530

    new file

---
+++ 01_JavaScript_Principles_Review.js
@@ -0,0 +1,56 @@
+
+            //----- (01) - (Executing JavaScript Code: A Review)
+
+// -- What happens when javascript executes (runs) my code?
+
+// const num = 3;
+// const multiplyBy2 = (inputNumber ) => {
+//     const result = inputNumber*2;
+//     return result;
+// }
+// const name = "Will";
+
+
+
+        // (02)Runnig/calling/invoking a function
+
+
+// const num = 3;
+// const multiplyBy2 = (inputNumber ) => {
+//     const result = inputNumber*2;
+//     return result;
+// }
+// const output = multiplyBy2(num); // 6
+// const newOutput = multiplyBy2(10); // 20
+
+// console.log(multiplyBy2(num))
+
+
+
+            //----- (02) - (Call Stack Review)
+
+
+// explain call stack
+// same code (running/calling/invoking)
+
+
+
+            //----- (03) - (Functions Review)
+
+
+// Suppose we have a function copyArrayAndMultiplyBy2.Let's diagram it out
+
+
+// const copyArrayAndMultiplyBy2 = ( array ) => {
+//     const output = [];
+//     for(let i = 0; i < array.length; i++){
+//         // output.push(array[i] * 2);
+//         output.push(array[i] / 2);  // -- Divide by 2
+//     }
+//     console.log(output)
+//     return output;
+// }
+// const myArray = [1,2,3];
+// const result = copyArrayAndMultiplyBy2(myArray);
+
+// // console.log(result)
---
+++ 02_Higher_Order_Functions.js
@@ -0,0 +1,15 @@
+
+            //----- (01) - (The Problem: Repetitive Functions)
+
+// What if want to copy array and divide by 2?
+
+
+const copyArrayAndDivideBy2 = (array) => {
+    const output = [];
+ for (let i = 0; i < array.length; i++) {
+    output.push(array[i] /2);
+ }
+ return output;
+}
+const myArray = [1,2,3]
+const result = copyArrayAndDivideBy2(myArray);
